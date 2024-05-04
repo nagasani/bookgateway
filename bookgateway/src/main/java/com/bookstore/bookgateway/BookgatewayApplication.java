3919be7cc2bde67ffa236eae36b765bac94d27a3
nagasani <nagasani.r@gmail.com>
2024-05-04 12:31:07 -0400

    Gateway Project

---
+++ bookgateway/src/main/java/com/bookstore/bookgateway/BookgatewayApplication.java
@@ -0,0 +1,13 @@
+package com.bookstore.bookgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookgatewayApplication.class, args);
+	}
+
+}
---
+++ bookgateway/src/test/java/com/bookstore/bookgateway/BookgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookstore.bookgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
